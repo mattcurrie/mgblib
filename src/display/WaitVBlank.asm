72867e03693ec10c69914ac159f994e7f7cbb2bc
Matt Currie <me@mattcurrie.com>
2022-12-20 18:23:03 +1300

    Add WaitVBlankIfLCDIsOn

--- src/display/WaitVBlank.asm
+++ src/display/WaitVBlank.asm
@@ -1,6 +1,16 @@
 IF !DEF(INC_WaitVBlank)
 INC_WaitVBlank = 1
 
+; Wait for Vblank (i.e. rLY = $90). Checks that the screen is not turned off
+; to prevent getting stuck in this function.
+;
+; @destroys af
+WaitVBlankIfLCDIsOn::
+    ; abort if screen is already off
+    ldh a, [rLCDC]
+    bit 7, a
+    ret z
+
 ; Wait for Vblank (i.e. rLY = $90).
 ;
 ; @destroys af
