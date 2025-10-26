a6242767495e05a72bf743d21a76c98d1abb8492
Matt Currie <me@mattcurrie.com>
2025-10-26 19:13:49 +1300

    Add palette macros

--- src/macros.asm
+++ src/macros.asm
@@ -36,6 +36,7 @@ MACRO wait_vram_accessible
 ; Enable GameBoy Color (CGB) mode
 ; Sets byte at address $0143 in the header to $80
 MACRO enable_cgb_mode
+DEF CGB_MODE = 1
 PUSHS
 SECTION "cgb-mode", ROM0[$143]
     db $80
@@ -50,6 +51,55 @@ POPS
 ; @param \3 blue (0 - 31)
 MACRO rgb
     dw (\1 + (\2 << 5) + \3 << 10)
+    ENDM
+
+
+; Outputs an instruction to load the low byte of a CGB palette value into A register
+; @param \1 red (0 - 31)
+; @param \2 green (0 - 31)
+; @param \3 blue (0 - 31)
+; @destroys a
+MACRO rgb_low_byte_ld
+    ld a, low(\1 + (\2 << 5) + \3 << 10)
+    ENDM
+
+
+; Outputs an instruction to load the high byte of a CGB palette value into A register
+;
+; @param \1 red (0 - 31)
+; @param \2 green (0 - 31)
+; @param \3 blue (0 - 31)
+; @destroys a
+MACRO rgb_high_byte_ld
+    ld a, high(\1 + (\2 << 5) + \3 << 10)
+    ENDM
+
+
+; Set a color in the background palette
+;
+; @param \1 red (0 - 31)
+; @param \2 green (0 - 31)
+; @param \3 blue (0 - 31)
+; @destroys a
+MACRO set_bg_palette
+    rgb_low_byte_ld \1, \2, \3
+    ldh [rBCPD], a
+    rgb_high_byte_ld \1, \2, \3
+    ldh [rBCPD], a    
+    ENDM
+
+
+; Set a color in the object palette
+;
+; @param \1 red (0 - 31)
+; @param \2 green (0 - 31)
+; @param \3 blue (0 - 31)
+; @destroys a
+MACRO set_obj_palette
+    rgb_low_byte_ld \1, \2, \3
+    ldh [rOCPD], a
+    rgb_high_byte_ld \1, \2, \3
+    ldh [rOCPD], a    
     ENDM
 
 
